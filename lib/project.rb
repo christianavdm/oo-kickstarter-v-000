465b67643aa9b40544d710e51aafcf3e28257159
Christiana Vandermale <christianavdm@gmail.com>
2018-05-17 00:01:41 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,15 @@
 class Project
   
   attr_accessor :title
+  attr_reader :backers
   
   def initialize(title) 
     @title = title
+    @backers = []
   end
   
   def add_backer(backer)
-    
+    @backers << backer
   end
   
 end
